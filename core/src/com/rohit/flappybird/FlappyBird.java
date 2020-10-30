e38db4e0c107089bc30c3268455ad4d47c72d255
Rohitn697 <53824509+Rohitn697@users.noreply.github.com>
2020-10-30 14:52:57 +0530

    bird can fly up and down now

--- core/src/com/rohit/flappybird/FlappyBird.java
+++ core/src/com/rohit/flappybird/FlappyBird.java
@@ -13,26 +13,52 @@ public class FlappyBird extends ApplicationAdapter {
 	Texture[] birds;
 
 	int flapstate = 0;
+	float birdY = 0;
+
+	float gravity = 2;
+	float velocity =0;
+	int gameState =0;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+
 		birds = new Texture[2];
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
+
+		birdY = (Gdx.graphics.getHeight())/2- birds[0].getHeight()/2;
 	}
 
 	@Override
 	public void render () {
-	if (flapstate==0){
-		flapstate=1;
-	}
-	else {
-		flapstate=0;
-	}
+		if(gameState != 0) {
+
+			if (Gdx.input.justTouched()) {
+				velocity = -30;
+			}
+
+			if (birdY>0 || velocity<0) {
+				velocity += gravity;
+				birdY -= velocity;
+			}
+		}
+		else {
+			if (Gdx.input.justTouched()) {
+			gameState=1;
+			}
+		}
+
+		if (flapstate == 0) {
+			flapstate = 1;
+		}
+		else {
+			flapstate = 0;
+		}
 		batch.begin();
-		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(birds[flapstate],(Gdx.graphics.getWidth())/2-birds[flapstate].getWidth()/2,(Gdx.graphics.getHeight())/2 - birds[flapstate].getHeight()/2);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birds[flapstate], (Gdx.graphics.getWidth()) / 2 - birds[flapstate].getWidth() / 2, birdY);
 		batch.end();
 	}
 	
